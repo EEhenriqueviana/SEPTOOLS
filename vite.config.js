a5db9595f4d260a0a5508ac49351f374e5dddc53
EEhenriqueviana <eng.henriqueviana@gmail.com>
2026-06-07 19:23:03 -0300

    gh pages 4

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/SEPTOOLS/',
+  build: {
+    chunkSizeWarningLimit: 600, // <-- Aumenta o limite para 600kB e esconde o aviso
+    rolldownOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
-
